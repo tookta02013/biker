a2f8b07616516f9871950d0729404ffb098d3c28
tookta02013 <32126783+tookta02013@users.noreply.github.com>
2017-12-15 16:34:51 +0700

    edit page JOB

--- src/pages/job/job.model.ts
+++ src/pages/job/job.model.ts
@@ -2,7 +2,19 @@ export class JobModel {
     _id: string;
     date: Date;
     shop: Array<ShopModel>;
+    customer : Array<CustomerModel>;
 }
 export class ShopModel {
-
+    name: string;
+    lat: string;
+    long: string;
+    address: string;
+    tel: string;
+}
+export class CustomerModel{
+    name: string;
+    lat: string;
+    long: string;
+    address: string;
+    tel: string;
 }
