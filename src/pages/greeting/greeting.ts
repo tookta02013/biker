39f2ca755c12b899f2ef94ece2b9565ddabba878
nicknameismos <mynameissarawut@gmail.com>
2018-01-08 18:54:04 +0700

    edit register

--- src/pages/greeting/greeting.ts
+++ src/pages/greeting/greeting.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from './../login/login';
+import { Component, ViewChild } from '@angular/core';
+import { Slides, IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the GreetingPage page.
@@ -14,12 +15,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'greeting.html',
 })
 export class GreetingPage {
+  lastSlide = false;
 
+  @ViewChild('slider') slider: Slides;
+  slideIndex = 0;
+  slides = [
+    {
+      title: 'Dream\'s Adventure',
+      imageUrl: './assets/imgs/Greeting1.png',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: './assets/imgs/Greeting2.png',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: './assets/imgs/Greeting3.png',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: './assets/imgs/Greeting4.png',
+      description: 'Take a look at our amazing options',
+    },
+    {
+      title: 'For the Weekend',
+      imageUrl: './assets/imgs/Greeting5.png',
+      description: 'Take a look at our amazing options',
+    }
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GreetingPage');
+  }
+  onSlideChanged() {
+    this.slideIndex = this.slider.getActiveIndex();
+    console.log('Slide changed! Current index is', this.slideIndex);
+  }
+
+  goToApp() {
+    this.navCtrl.setRoot(LoginPage);
+    console.log('Go to App clicked');
+  }
+  skip() {
+    this.lastSlide = true;
+    this.slider.slideTo(this.slider.length());
   }
 
 }
